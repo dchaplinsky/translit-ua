d0093f3d38eb3465f76c3ac8c86d06069c1a1627
enagorny <ideviantik@gmail.com>
2014-11-18 22:59:06 +0000

    nominal 2.6 support

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
     ],
 
